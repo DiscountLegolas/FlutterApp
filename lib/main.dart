686c9ae89657a5db0574ca9cab93a9fdbe49b18c
FlutLab.io <>
2022-04-12 10:20:27 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class PostPage extends StatelessWidget {
                 Text(post.Title),
                 const SizedBox(height: 20),
                 ConstrainedBox(
-                    constraints: BoxConstraints(minHeight: 100, maxHeight: (MediaQuery.of(context).size.height / 10) * 7),
+                    constraints: BoxConstraints(minHeight: 100, maxHeight: (MediaQuery.of(context).size.height / 10) * 6),
                     child: SingleChildScrollView(
                       child: Text(post.Content),
                     )),
