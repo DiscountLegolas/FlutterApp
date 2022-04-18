d6a191d4d92862109b247bb203d9bdd8ae1d1a0c
FlutLab.io <>
2022-04-18 12:30:39 +0000

    Autosave

--- lib/Api/Models/Therapist.dart
+++ lib/Api/Models/Therapist.dart
@@ -36,7 +36,7 @@ class Therapist extends BaseUser {
                   ));
             },
             child: Container(
-              height: 75,
+              height: 100,
               decoration: BoxDecoration(border: Border.all(color: Colors.amber)),
               child: Padding(
                 padding: EdgeInsets.all(20.0),
