7f691d3d06503e4ae25bedcbc96bc2094c3f2add
FlutLab.io <>
2022-04-18 09:32:29 +0000

    Autosave

--- lib/Provider/Pages/PostsPage.dart
+++ lib/Provider/Pages/PostsPage.dart
@@ -35,6 +35,7 @@ class PostsGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var post = context.watch<PostsModel>();
+    post.GetAllPosts();
     // TODO: implement build
     return Column(children: <Widget>[
       Container(
